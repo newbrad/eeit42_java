b6f23dfed434f2e41ec3a95b52bba9534dd7472c
USER <brad@brad.tw>
2022-03-11 09:56:54 +0800

    03110956

--- MyBrad/src/tw/brad/tutor/Brad41.java
+++ MyBrad/src/tw/brad/tutor/Brad41.java
@@ -5,7 +5,6 @@ import tw.brad.myclass.TWId;
 public class Brad41 {
 
 	public static void main(String[] args) {
-		System.out.println(Math.random());
 		if (TWId.isValid("A123456789")) {
 			System.out.println("OK");
 		}else {
