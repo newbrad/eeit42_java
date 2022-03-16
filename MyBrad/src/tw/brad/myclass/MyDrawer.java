cff09643251837b643d51d61f91027422412d8ad
USER <brad@brad.tw>
2022-03-16 16:31:25 +0800

    03161631

--- MyBrad/src/tw/brad/myclass/MyDrawer.java
+++ MyBrad/src/tw/brad/myclass/MyDrawer.java
@@ -1,12 +1,47 @@
 package tw.brad.myclass;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel {
+	private LinkedList<HashMap<String,Integer>> line;
+	
 	public MyDrawer() {
 		setBackground(Color.YELLOW);
+		MyListener myListener = new MyListener();
+		addMouseListener(myListener);
+		addMouseMotionListener(myListener);
+		line = new LinkedList<>();
 	}
-
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setColor(Color.BLUE);
+		g2d.setStroke(new BasicStroke(4));
+		g2d.drawLine(0, 0, 200, 200);
+		
+	}
+	
+	private class MyListener extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			System.out.println("Press:" + e.getX() + " x " + e.getY());
+		}
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			System.out.println("Drag:" + e.getX() + " x " + e.getY());
+		}
+	}
+	
 }
